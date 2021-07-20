1b507277e7601c97f5c3eb6df73b4ea88cf69e6e
adrianlukito <lukito.adrian1@gmail.com>
2021-07-20 16:01:27 +0700

    Setup Movie RecyclerView Adapter

---
+++ app/src/main/java/com/example/movieapplication/ui/main/MovieListAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.movieapplication.ui.main
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movieapplication.databinding.ItemMovieBinding
+
+class MovieListAdapter(val context: Context, private val movies: List<String>): RecyclerView.Adapter<MovieListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.cvMovie.setOnClickListener {
+            Toast.makeText(context, "CLICKED $position", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    inner class ViewHolder(val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root)
+}
--- app/src/main/java/com/example/movieapplication/ui/main/popular/PopularFragment.kt
+++ app/src/main/java/com/example/movieapplication/ui/main/popular/PopularFragment.kt
@@ -4,12 +4,20 @@ import android.view.View
 import com.example.movieapplication.R
 import com.example.movieapplication.databinding.FragmentPopularBinding
 import com.example.movieapplication.ui.BaseFragment
+import com.example.movieapplication.ui.main.MovieListAdapter
 
 class PopularFragment: BaseFragment<FragmentPopularBinding>() {
 
     override fun getContentResource(): Int = R.layout.fragment_popular
 
+    private val adapter by lazy {
+        MovieListAdapter(requireContext(), listOf("asd", "dsa", "asd", "dsa", "asd", "dsa"))
+    }
+
     override fun setupViewBinding(view: View) {
         binding = FragmentPopularBinding.bind(view)
+        binding?.run {
+            rvPopularMovies.adapter = adapter
+        }
     }
 }
