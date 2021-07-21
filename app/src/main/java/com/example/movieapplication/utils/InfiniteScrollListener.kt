8630f53bddce653facc1732a82b0fd31aee8a802
adrianlukito <lukito.adrian1@gmail.com>
2021-07-21 18:22:56 +0700

    Setup Infinite Scroll Listener

---
+++ app/src/main/java/com/example/movieapplication/utils/InfiniteScrollListener.kt
@@ -0,0 +1,49 @@
+package com.example.movieapplication.utils
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class InfiniteScrollListener(
+    private val layoutManager: LinearLayoutManager,
+    private var startPage: Int = Constants.START_PAGE,
+    private var visibleThreshold: Int = 2,
+    private val onLoad: (page: Int) -> Unit
+): RecyclerView.OnScrollListener() {
+
+    private var previousTotalItemCount = 0
+    private var loading = true
+    private var firstVisibleItem = 0
+    private var visibleItemCount = 0
+    private var totalItemCount = 0
+    private var currentPage = 0
+
+    init {
+        currentPage = startPage
+    }
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+        if(dy > 0) {
+            visibleItemCount = recyclerView.childCount
+            totalItemCount = layoutManager.itemCount
+            firstVisibleItem = layoutManager.findFirstVisibleItemPosition()
+
+            if(loading && totalItemCount > previousTotalItemCount) {
+                loading = false
+                previousTotalItemCount = totalItemCount
+                currentPage++
+            }
+
+            if(!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+                loading = true
+                onLoad.invoke(currentPage)
+            }
+        }
+    }
+
+    fun resetState() {
+        currentPage = startPage
+        previousTotalItemCount = 0
+        loading = true
+    }
+}
