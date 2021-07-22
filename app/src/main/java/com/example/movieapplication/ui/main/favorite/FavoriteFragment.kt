744ab7c7b854477ac77e2b6a0ccd3df0dbb5f038
adrianlukito <lukito.adrian1@gmail.com>
2021-07-22 16:13:42 +0700

    Integrate FavoriteMovies page with API

--- app/src/main/java/com/example/movieapplication/ui/main/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/movieapplication/ui/main/favorite/FavoriteFragment.kt
@@ -1,12 +1,22 @@
 package com.example.movieapplication.ui.main.favorite
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.view.isVisible
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.movieapplication.R
 import com.example.movieapplication.databinding.FragmentFavoriteBinding
+import com.example.movieapplication.model.Movie
+import com.example.movieapplication.model.Resource
 import com.example.movieapplication.ui.BaseFragment
+import com.example.movieapplication.ui.main.MovieListAdapter
 import com.example.movieapplication.ui.main.popular.PopularViewModel
+import com.example.movieapplication.utils.Constants
+import com.example.movieapplication.utils.InfiniteScrollListener
 import com.example.movieapplication.viewmodels.ViewModelProvidersFactory
 import javax.inject.Inject
 
@@ -19,6 +29,24 @@ class FavoriteFragment: BaseFragment<FragmentFavoriteBinding>() {
 
     override fun getContentResource(): Int = R.layout.fragment_favorite
 
+    private val adapter by lazy {
+        MovieListAdapter(requireContext()) {
+            goToDetail()
+        }
+    }
+
+    private val scrollListener by lazy {
+        InfiniteScrollListener(binding?.rvFavoriteMovies?.layoutManager as GridLayoutManager) {
+            Log.d("lolo", ": $it")
+            viewModel.currentPage = it
+            viewModel.getFavoriteMovies(it)
+        }
+    }
+
+    private fun goToDetail() {
+        NavHostFragment.findNavController(this).navigate(R.id.goToDetail)
+    }
+
     override fun setupViewBinding(view: View) {
         binding = FragmentFavoriteBinding.bind(view)
     }
@@ -30,6 +58,47 @@ class FavoriteFragment: BaseFragment<FragmentFavoriteBinding>() {
     }
 
     private fun subscribeObserver() {
-        // call view model
+        viewModel.moviesLiveData.observe(viewLifecycleOwner, Observer {
+            when (it.status) {
+                Resource.Status.LOADING -> handleLoading()
+                Resource.Status.SUCCESS -> it.data?.let { handleSuccess(it.results) }
+                Resource.Status.ERROR -> handleError()
+            }
+        })
+    }
+
+    private fun handleLoading() {
+        binding?.run {
+            if(viewModel.currentPage == Constants.START_PAGE) {
+                loading.isVisible = true
+                rvFavoriteMovies.isVisible = false
+                tvError.isVisible = false
+            } else {
+                footerLoading.isVisible = true
+            }
+        }
+    }
+
+    private fun handleSuccess(item: List<Movie>) {
+        binding?.run {
+            if(viewModel.currentPage == Constants.START_PAGE) {
+                adapter.setData(item)
+            } else {
+                adapter.addMore(item)
+            }
+            rvFavoriteMovies.isVisible = true
+            loading.isVisible = false
+            tvError.isVisible = false
+            footerLoading.isVisible = false
+        }
+    }
+
+    private fun handleError() {
+        binding?.run {
+            loading.isVisible = false
+            rvFavoriteMovies.isVisible = false
+            tvError.isVisible = true
+            footerLoading.isVisible = false
+        }
     }
 }
--- app/src/main/java/com/example/movieapplication/ui/main/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/example/movieapplication/ui/main/favorite/FavoriteViewModel.kt
@@ -1,11 +1,37 @@
 package com.example.movieapplication.ui.main.favorite
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.movieapplication.model.Movies
+import com.example.movieapplication.model.Resource
 import com.example.movieapplication.network.movie.MovieApi
+import com.example.movieapplication.utils.Constants
+import com.example.movieapplication.utils.SchedulerTransformer
+import com.example.movieapplication.utils.customSubscribe
+import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
 class FavoriteViewModel @Inject constructor(
     private val movieApi: MovieApi
 ) : ViewModel() {
+    private val disposable = CompositeDisposable()
 
+    internal var currentPage = Constants.START_PAGE
+
+    private val _moviesLiveData = MutableLiveData<Resource<Movies>>()
+    val moviesLiveData: LiveData<Resource<Movies>> = _moviesLiveData
+
+    fun getFavoriteMovies(page: Int = Constants.START_PAGE) {
+        _moviesLiveData.value = Resource.loading(null)
+        disposable.add(movieApi.getFavoriteMovies(page = page).compose(SchedulerTransformer()).customSubscribe({
+            _moviesLiveData.value = Resource.success(it)
+        }, {
+            _moviesLiveData.value = it.message?.let { it1 -> Resource.error(it1, null) }
+        }))
+    }
+
+    override fun onCleared() {
+        disposable.clear()
+    }
 }
